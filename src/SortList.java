562382efba0a334916961e5f5ccf9ac228a4b935
ntsdc <mdznpc@gmail.com>
2023-03-17 14:18:15 +0800

    3.17

--- src/SortList.java
+++ src/SortList.java
@@ -1,35 +1,60 @@
 import ds.ListNode;
+import ds.String2Array;
 
 public class SortList {
 
     public static void main(String[] args) {
+
+        System.out.println(
+
+                String2Array.listNodeToString(
+                        new Solution148().sortList(
+                                String2Array.stringToListNode(
+                                        "[4,2,1,3]"
+                                )
+                )
+
+
+        ));
+
 
     }
 
 }
 
 //reuse of solution 21
-class Solution {
-    public ListNode sortList(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
+class Solution148 {
+
+    public ListNode sortList(ListNode head){
+        return sortList(head, null);
+    }
+
+
+    public ListNode sortList(ListNode head, ListNode tail) {
         if(head == null){
             return null;
         }
-        if(head.next == null){
+        if(head.next == tail){
+            head.next = null;
             return head;
         }
-        while(fast.next != null){
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != tail){
             slow = slow.next;
-            fast = fast.next.next;
+            fast = fast.next;
+            if(fast != tail){
+                fast = fast.next;
+            }
         }
-        ListNode rightStart = slow.next;
-        slow.next = null;
-        ListNode left = sortList(head);
-        ListNode right = sortList(rightStart);
+        ListNode mid = slow;
+
+        ListNode left = sortList(head, mid);
+        ListNode right = sortList(mid, tail);
 
         return new Solution21().mergeTwoLists(left, right);
-
 
     }
 }
