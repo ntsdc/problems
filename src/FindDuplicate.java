48d31f4c8b8543f40231d7e81b3ad09dd9e359e3
ntsdc <mdznpc@gmail.com>
2023-03-28 11:58:12 +0800

    3.28

---
+++ src/FindDuplicate.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+public class FindDuplicate {
+
+}
+
+class Solution287 {
+    public int findDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i++) {
+            if(nums[i] == nums[i + 1]){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+}
+
+class Solution287_ {  //142
+    public int findDuplicate(int[] nums) {
+        int slow = 0;
+        int fast = 0;
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        int pre1 = 0;
+        int pre2 = slow;
+        while(pre1 != pre2){
+            pre1 = nums[pre1];
+            pre2 = nums[pre2];
+        }
+        return pre1;
+    }
+}
